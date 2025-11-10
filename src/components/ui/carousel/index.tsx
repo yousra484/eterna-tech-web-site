fe95bd64874d70e1b7f610dc06b45575de167be3
yousra484 <yousra484@github.com>
2025-11-10 16:42:09 +0100

    Remove source files, keep only built production files

--- src/components/ui/carousel/index.tsx
+++
@@ -1,7 +0,0 @@
-
-export { Carousel } from "./carousel"
-export { CarouselContent } from "./carousel-content"
-export { CarouselItem } from "./carousel-item"
-export { CarouselNext } from "./carousel-next"
-export { CarouselPrevious } from "./carousel-previous"
-export { type CarouselApi } from "./carousel-context"
